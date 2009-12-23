b1dd131f7c1a02e7ffe0d36e96c48363f8c8e870
Brandon Williams <brandon@racklabs.com>
2009-12-23 16:19:58 -0600

    setup.py and debian packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+setup(
+    name='telephus',
+    version='0.5',
+    description='connection pooled, low-level client API for Cassandra in Twisted python',
+    author='brandon@faltering.com',
+    url='http://github.com/driftx/Telephus',
+    packages=['telephus', 'telephus.cassandra']
+)
