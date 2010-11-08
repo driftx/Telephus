c652bae3a68eff420101ca92945a3df3a7717617
paul cannon <paul@riptano.com>
2010-11-08 16:07:13 -0600

    Release 0.7~beta3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(
     name='telephus',
-    version='0.7~beta3.2',
+    version='0.7~beta3.3',
     description='connection pooled, low-level client API for Cassandra in Twisted python',
     author='brandon@faltering.com',
     url='http://github.com/driftx/Telephus',
