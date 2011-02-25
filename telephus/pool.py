b6bfd0816edf92dc2dda74ed1c18c8613e4ddb6d
paul cannon <paul@riptano.com>
2011-02-24 21:21:43 -0600

    "quick start" module docstring

--- telephus/pool.py
+++ telephus/pool.py
@@ -2,6 +2,44 @@
 #
 # spread requests among connections to multiple nodes in a Cassandra cluster
 
+"""
+Quick start:
+
+>>> my_seed_nodes = ['192.168.2.14', '192.168.2.15', '192.168.2.16']
+>>> mypool = CassandraClusterPool(my_seed_nodes, keyspace='MyKeyspace', pool_size=10)
+>>> mypool.startService()
+>>> mypool.get('Key12345', 'SomeCF')
+<Deferred at 0x1b2b248>
+
+CassandraClusterPool will respond to all the methods on CassandraClient, but
+if you prefer to have separate CassandraClient instances, set your pool object
+as their manager.
+
+Some of the most useful additional methods on CassandraClusterPool:
+
+   adjustPoolSize(newsize)      # change the size of the connection pool,
+                                #   without interrupting ongoing requests
+   addNode((address, port))     # manually add another node to the pool.
+                                #   Normally this shouldn't be necessary; once
+                                #   the pool can connect to one or more of your
+                                #   your seed nodes, it can inspect the ring
+                                #   and find the rest of the nodes.
+   removeNode((address, port))  # manually remove a node from the pool. It will
+                                #   be re-added later if it shows up in the
+                                #   ring with a subsequent connection, though.
+   set_keyspace(ksname)         # change the keyspace used for future requests
+                                #   on this pool
+
+TODO:
+
+    * check cluster name on connecting to each new host, to make sure it's
+      actually in the same cluster
+    * take node error/connection history into account with add_connection_score
+    * remove nodes that have been missing or unconnectable for too long
+    * don't re-add manually removed nodes
+
+"""
+
 import sys
 import random
 import socket
