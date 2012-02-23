1b6fae266515fc8d006922febf4616b74e0e3e03
John Wehr <johnwehr@gmail.com>
2012-02-23 01:06:11 -0500

    Version number once again giving SetupTools fits.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 setup(
     name='telephus',
-    version='1.0.0_beta1',
+    version='1.0.0beta1',
     description='connection pooled, low-level client API for Cassandra in Twisted python',
     author='brandon@faltering.com',
     url='http://github.com/driftx/Telephus',
