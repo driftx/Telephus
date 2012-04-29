2d57622557f574d082a8c6675920c7b5db8b848b
Duncan McGreggor <duncan@dreamhost.com>
2012-04-29 00:33:01 -0400

    Fixed long lines.

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 from twisted.internet import defer
 
-from telephus.cassandra.ttypes import ColumnPath, ColumnParent, Column, SuperColumn
+from telephus.cassandra.ttypes import (
+    ColumnPath, ColumnParent, Column, SuperColumn)
 from telephus.client import CassandraClient
 from telephus.protocol import ManagedCassandraClientFactory
 
@@ -19,19 +20,24 @@ scname = 'bar'
 
 @defer.inlineCallbacks
 def dostuff(client):
-    yield client.insert(key='test', column_family=CF, value='testval', column=colname)
-    yield client.insert(key='test', column_family=SCF, value='testval', column=colname, super_column=scname)
+    yield client.insert(
+        key='test', column_family=CF, value='testval', column=colname)
+    yield client.insert(
+        key='test', column_family=SCF, value='testval', column=colname,
+        super_column=scname)
 
     res = yield client.get(key='test', column_family=CF, column=colname)
     print 'get', res
 
-    res = yield client.get(key='test', column_family=SCF, column=colname, super_column=scname)
+    res = yield client.get(
+        key='test', column_family=SCF, column=colname, super_column=scname)
     print 'get (super)', res
 
     res = yield client.get_slice(key='test', column_family=CF)
     print 'get_slice', res
 
-    res = yield client.multiget(keys=['test', 'test2'], column_family=CF, column=colname)
+    res = yield client.multiget(
+        keys=['test', 'test2'], column_family=CF, column=colname)
     print 'multiget', res
 
     res = yield client.multiget_slice(keys=['test', 'test2'], column_family=CF)
@@ -40,21 +46,27 @@ def dostuff(client):
     res = yield client.get_count(key='test', column_family=CF)
     print 'get_count', res
 
-    yield client.add(key='test', column_family=COUNT_CF, value=1, column='testcounter')
-    res = yield client.get(key='test', column_family=COUNT_CF, column='testcounter')
+    yield client.add(
+        key='test', column_family=COUNT_CF, value=1, column='testcounter')
+    res = yield client.get(
+        key='test', column_family=COUNT_CF, column='testcounter')
     print 'get counter value', res
 
-    yield client.add(key='test', column_family=SUPERCOUNT_CF, value=1,
-                     column='testcounter', super_column='testsuper')
-    res = yield client.get(key='test', column_family=SUPERCOUNT_CF,
-                           column='testcounter', super_column='testsuper')
+    yield client.add(
+        key='test', column_family=SUPERCOUNT_CF, value=1, column='testcounter',
+        super_column='testsuper')
+    res = yield client.get(
+        key='test', column_family=SUPERCOUNT_CF, column='testcounter',
+        super_column='testsuper')
     print 'get super counter value', res
 
     # batch insert will figure out if you're trying a CF or SCF
     # from the data structure
-    res = yield client.batch_insert(key='test', column_family=CF, mapping={colname: 'bar'})
+    res = yield client.batch_insert(
+        key='test', column_family=CF, mapping={colname: 'bar'})
     print "batch_insert", res
-    res = yield client.batch_insert(key='test', column_family=SCF, mapping={'foo': {colname: 'bar'}})
+    res = yield client.batch_insert(
+        key='test', column_family=SCF, mapping={'foo': {colname: 'bar'}})
     print "batch_insert", res
 
     # with ttypes, you pass a list as you would for raw thrift
@@ -71,11 +83,11 @@ def dostuff(client):
 
 
 if __name__ == '__main__':
+    import sys
     from twisted.internet import reactor
     from twisted.python import log
-    import sys
-    log.startLogging(sys.stdout)
 
+    log.startLogging(sys.stdout)
     f = ManagedCassandraClientFactory(KEYSPACE)
     c = CassandraClient(f)
     dostuff(c)
