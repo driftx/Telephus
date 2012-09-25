5424645c854e7deb83b3e85fbbcf90e664ccc2cb
Tyler Hobbs <tyler@datastax.com>
2012-09-25 15:17:23 -0500

    Handle schema setup in sasl demo

--- sasl_demo.py
+++ sasl_demo.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python
 from telephus.protocol import ManagedCassandraClientFactory
 from telephus.client import CassandraClient
-from telephus.cassandra.ttypes import Column, SuperColumn
+from telephus.cassandra.ttypes import Column, KsDef, CfDef
 from twisted.internet import defer
 
 HOST = 'localhost'
 PORT = 9160
 KEYSPACE = 'Keyspace1'
 CF = 'Standard1'
-SCF = 'Super1'
-COUNT_CF = 'Counter1'
-SUPERCOUNT_CF = 'SuperCounter1'
 colname = 'foo'
 scname = 'bar'
 
 @defer.inlineCallbacks
 def dostuff(client):
     yield client.insert(key='test', column_family=CF, value='testval', column=colname)
-    yield client.insert(key='test', column_family=SCF, value='testval', column=colname, super_column=scname)
 
     res = yield client.get(key='test', column_family=CF, column=colname)
     print 'get', res
-
-    res = yield client.get(key='test', column_family=SCF, column=colname, super_column=scname)
-    print 'get (super)', res
 
     res = yield client.get_slice(key='test', column_family=CF)
     print 'get_slice', res
@@ -37,21 +30,9 @@ def dostuff(client):
     res = yield client.get_count(key='test', column_family=CF)
     print 'get_count', res
 
-    yield client.add(key='test', column_family=COUNT_CF, value=1, column='testcounter')
-    res = yield client.get(key='test', column_family=COUNT_CF, column='testcounter')
-    print 'get counter value', res
-
-    yield client.add(key='test', column_family=SUPERCOUNT_CF, value=1,
-                     column='testcounter', super_column='testsuper')
-    res = yield client.get(key='test', column_family=SUPERCOUNT_CF,
-                           column='testcounter', super_column='testsuper')
-    print 'get super counter value', res
-
     # batch insert will figure out if you're trying a CF or SCF
     # from the data structure
     res = yield client.batch_insert(key='test', column_family=CF, mapping={colname: 'bar'})
-    print "batch_insert", res
-    res = yield client.batch_insert(key='test', column_family=SCF, mapping={'foo': {colname: 'bar'}})
     print "batch_insert", res
 
     # with ttypes, you pass a list as you would for raw thrift
@@ -59,11 +40,37 @@ def dostuff(client):
     cols = [Column(colname, 'bar', 1234), Column('bar', 'baz', 54321)]
     res = yield client.batch_insert(key='test', column_family=CF, mapping=cols)
     print "batch_insert", res
-    cols = [SuperColumn(name=colname, columns=cols)]
 
-    # of course you don't have to use kwargs if the order is correct
-    res = yield client.batch_insert('test', SCF, cols)
-    print "batch_insert", res
+    client.system_drop_keyspace('Keyspace1')
+
+
+@defer.inlineCallbacks
+def setup_schema(client):
+    print "setting up schema"
+    ks_defs = yield client.describe_keyspaces()
+    if 'Keyspace1' not in [k.name for k in ks_defs]:
+        yield client.system_add_keyspace(KsDef('Keyspace1', 'SimpleStrategy', {'replication_factor': '1'}, cf_defs=[]))
+        yield client.set_keyspace('Keyspace1')
+        yield client.system_add_column_family(CfDef('Keyspace1', 'Standard1'))
+        print "schema created"
+    else:
+        print "schema already exists"
+        yield client.set_keyspace('Keyspace1')
+
+
+@defer.inlineCallbacks
+def main():
+    sasl_kwargs = {
+            "sasl_host": "thobbs-laptop2",
+            "sasl_service": "host",
+            "mechanism": "GSSAPI"}
+    f = ManagedCassandraClientFactory(keyspace='system', sasl_kwargs=sasl_kwargs)
+    reactor.connectTCP(HOST, PORT, f)
+    yield f.deferred
+    c = CassandraClient(f)
+
+    yield setup_schema(c)
+    yield dostuff(c)
 
 
 if __name__ == '__main__':
@@ -71,13 +78,5 @@ if __name__ == '__main__':
     from twisted.python import log
     import sys
     log.startLogging(sys.stdout)
-
-    sasl_kwargs = {
-            "sasl_host": "thobbs-laptop2",
-            "sasl_service": "host",
-            "mechanism": "GSSAPI"}
-    f = ManagedCassandraClientFactory(keyspace=KEYSPACE, sasl_kwargs=sasl_kwargs)
-    c = CassandraClient(f)
-    dostuff(c)
-    reactor.connectTCP(HOST, PORT, f)
+    main()
     reactor.run()
